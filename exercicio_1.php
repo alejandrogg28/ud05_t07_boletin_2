c7838c684e17ec55ca146fe7adfcfee77020d8b0
Alejandro Garcia <alejandrogg28@gmail.com>
2022-02-03 13:18:17 +0100

    Boletin 2 Ejercicio 1

--- exercicio_1.php
+++ exercicio_1.php
@@ -3,6 +3,7 @@
 
 header('Content-Type: text/html; charset=UTF-8');
 echo "<pre>";print_r($_REQUEST); echo "</pre>";
-echo "O valor do primeiro número é: ".$_REQUEST[num1];
-echo "O valor do segundo número é: ".$_REQUEST[num2];
+echo "<br/>O valor do primeiro número é: ". "  ".$_REQUEST[num1];
+echo "<br/>O valor do segundo número é: ". "  ".$_REQUEST[num2];
+echo "<br/>O resultado da multiplicación é: ".$_REQUEST[num1] * $_REQUEST[num2];
 ?>

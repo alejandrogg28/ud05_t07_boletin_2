f63c27c74127163d6be4de70ad3a37187a76f044
Alejandro Garcia <alejandrogg28@gmail.com>
2022-02-07 09:53:13 +0100

    Ejercicio 5 Boletín 2

---
+++ exercicio_5.php
@@ -0,0 +1,9 @@
+<?php
+
+//Escribe un programa que calcule el área de un rectángulo. base * altura
+
+$base=$_REQUEST["base"];
+$altura=$_REQUEST["altura"];
+
+echo "<br/>El rectángulo tiene un área de ".$_REQUEST["base"] * $_REQUEST["altura"];
+?>
